2040365b2412026a494aafe5bf7fff8c7956b768
Dmitry Home <di@goldcarrot.ru>
2018-03-14 21:49:15 +0500

    inprogress

--- js/java.js
+++ js/java.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){ 
-  // new WOW().init();
-
+	// new WOW().init();
+	$(document).on('click', '.video-wrap .play-button', function(){
+		$(this).fadeOut('fast');
+		$(this).prev()[0].play();
+	})
+	$(document).on('click', '.tab-button', function () {
+		$(this).parent().find('.tab-button').removeClass('active');
+		$(this).addClass('active');
+	})
+	$(document).on('click', '.tab-button', function(){
+        console.log(123)
+        $(this).parents('.tabs').find('.tabs-content > div').hide();
+		$(this).parents('.tabs').find('.content-'+$(this).data('id')).fadeIn('fast');
+	});
 })
